0a7f7267cbfdd053f042b675f20fad878a4c27cf
Shiina Sugioka <shiina12@uw.edu>
2024-04-16 11:22:27 -0700

    init app

---
+++ Life CounterUITests/Life_CounterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Life_CounterUITestsLaunchTests.swift
+//  Life CounterUITests
+//
+//  Created by stlp on 4/16/24.
+//
+
+import XCTest
+
+final class Life_CounterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
